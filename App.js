58143ca239305cf82a9475f94cbe12c1c8493387
mohansrichollangi <chollangi.m@northeastern.edu>
2024-04-15 19:17:23 -0400

    users changes

--- App.js
+++ App.js
@@ -1,22 +1,60 @@
-import express from 'express';
-import Hello from "./Hello.js"
-import Lab5 from './Lab5.js';
-import cors from "cors";
+// const express= require('express');
+import "dotenv/config";
+import express from "express";
+import morgan from "morgan";
+import Hello from "./Hello.js";
+import Lab5 from "./Lab5.js";
 import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/modules/routes.js";
-import AssignmentRoutes from './Kanbas/assignments/routes.js';
+import AssignmentRoutes from "./Kanbas/assignments/routes.js";
+import UserRoutes from "./Users/routes.js";
+import cors from "cors";
+import session from "express-session";
+import mongoose from "mongoose";
 
-// const express = require('express')
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas'
+mongoose.connect(CONNECTION_STRING);
+
 const app = express();
-app.use(cors());
+
+app.use(morgan("dev"));
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+  })
+);
+
+const sessionOptions = {
+  secret: "keyboardcat",
+  resave: false,
+  saveUninitialized: true,
+};
+
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+  };
+}
+
 app.use(express.json());
-AssignmentRoutes(app);
+app.use(session(sessionOptions));
+
+app.get("/", (_, res) => {
+  res.send({ status: "ok" });
+});
+
 ModuleRoutes(app);
 CourseRoutes(app);
-// app.get('/hello', (req, res) => {res.send('Life is good!')})
-// app.get('/', (req, res) => {res.send('Welcome to Full Stack Development!')})
-Hello(app);
+AssignmentRoutes(app);
 Lab5(app);
-// app.listen(4000);
-app.listen(process.env.PORT || 4000);
+Hello(app);
+UserRoutes(app);
 
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, function () {
+  console.log(`Server is running on port ${PORT}`);
+});
