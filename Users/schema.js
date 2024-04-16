b1b46a56912079e909207cd70ecc5310297fef56
mohansrichollangi <chollangi.m@northeastern.edu>
2024-04-15 20:12:44 -0400

    cors changes

--- Users/schema.js
+++ Users/schema.js
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
 const userSchema = new mongoose.Schema(
   {
-    id: String,
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     firstName: String,
